4acc47152e7182955a730ab19efe7367d7176390
tonydevpro <antonio.1358@hotmail.com>
2025-05-15 18:48:18 -0300

    .

--- postes.js
+++ postes.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose');
-const uri = "mongodb+srv://tonyDev:antonio1358@cluster0.zsmnsv3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://tonyDev:antonio1358@cluster0.zsmnsv3.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0";
 
 
 
